a1a83402f4afe68d5394e95fea1754116ee48121
martinski74 <martin.dobr@abv.bg>
2025-05-06 21:28:23 +0300

    host fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    compress: true,
     port: process.env.PORT,
+    allowedHosts: 'all',
   },
 });
